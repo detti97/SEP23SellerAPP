b152e633a870d9100e68beebdb1cd5b6c4f8081b
jan dettler <jan.dettler@hs-osnabrueck.de>
2023-04-07 18:46:28 +0200

    Initial Commit

---
+++ SEP23SellerAPP/SEP23SellerAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SEP23SellerAPPApp.swift
+//  SEP23SellerAPP
+//
+//  Created by Jan Dettler on 07.04.23.
+//
+
+import SwiftUI
+
+@main
+struct SEP23SellerAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
